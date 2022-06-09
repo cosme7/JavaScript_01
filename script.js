dd8666292f07fbe8aa38ca12bc684282c3f3a0f8
cosme7 <cosmewr@gmail.com>
2022-06-08 21:48:38 -0300

    100%

---
+++ script.js
@@ -0,0 +1,15 @@
+// Crie uma variável que receba um nome;
+let nome = "Cosme";
+// Crie uma variável que receba um número;
+let number = 1984;
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let ano = 2022;
+console.log(ano - number);
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(ano + number);
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log");
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true;
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
